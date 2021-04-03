6ddac8f33316100b075462c33ab03f5ecc55fdd2
jlafer <jlafer@twilio.com>
2021-04-03 15:19:03 -0700

    initial commit

---
+++ src/TemplatePlugin.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { VERSION } from '@twilio/flex-ui';
+import { FlexPlugin } from 'flex-plugin';
+
+import reducers, { namespace } from './states';
+
+const PLUGIN_NAME = 'TemplatePlugin';
+
+export default class TemplatePlugin extends FlexPlugin {
+  constructor() {
+    super(PLUGIN_NAME);
+  }
+
+  init(flex, manager) {
+    this.registerReducers(manager);
+  }
+
+  registerReducers(manager) {
+    if (!manager.store.addReducer) {
+      // eslint: disable-next-line
+      console.error(`You need FlexUI > 1.9.0 to use built-in redux; you are currently on ${VERSION}`);
+      return;
+    }
+    manager.store.addReducer(namespace, reducers);
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import * as FlexPlugin from 'flex-plugin';
+import TemplatePlugin from './TemplatePlugin';
+
+FlexPlugin.loadPlugin(TemplatePlugin);
